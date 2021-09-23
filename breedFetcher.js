f46f9fcf21eb8e4f0371a9293d218000459f6d8e
Ahmed Tarabia <ahmedtarabia@gmail.com>
2021-09-23 16:12:24 -0400

    mocha / chai tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,20 @@
 //require request
 const request = require('request');
-let breedName = process.argv[2];
 
+const fetchBreedDescription = function(breedName, callback) {
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+  request(url, (error, response, body) => {
+    if (error) { //request failure
+      return callback(error, null);
+    }
 
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
+    const data = JSON.parse(body);
+    if (data[0]) { //breed there
+      callback(null, data[0].description);
+    } else { // name not found
+      callback("breed not found!", null);
+    }
+  });
+};
 
-request(url, (error, response, body) => {
-  
-  if (error) { //request failure
-    return console.log('error:', error);
-  }
-  const data = JSON.parse(body);
-  if (data[0]) { //breed there
-    console.log(data[0].description);
-  } else { // name not found
-    console.log("breed not found!");
-  }
-});
-
-
-
-
+module.exports = { fetchBreedDescription };
